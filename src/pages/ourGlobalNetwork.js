bad8f5221c50abd08b84e78cc8ba9406765d2ba4
Berlik Gabdulin <berlik.gabdulin@gmail.com>
2023-05-12 17:01:21 +0600

    ourNetwork static array was removed, getting this data dynamicly from backend, using partners array

--- src/pages/ourGlobalNetwork.js
+++ src/pages/ourGlobalNetwork.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { SVGMap } from 'react-svg-map';
 import 'react-svg-map/lib/index.css';
 import World from '@svg-maps/world';
-import { eurasia, ourNetwork } from '../components/network';
+import { eurasia } from '../components/network';
 import { useSelector } from 'react-redux';
 import { Partner } from '../components/partnerCard';
 import { scrollTo } from '../libs/scrollTo';
@@ -14,8 +14,14 @@ export const OurGlobalNetwork = () => {
     return state.partners;
   });
   const [country, setCountry] = useState('Select country');
+  const [ourNetwork, setOurNetwork] = useState([]);
   const [showPartner, setShowPartner] = useState(false);
   const [curPartner, setCurPartner] = useState([]);
+  const map = document.querySelector('.svg-map');
+
+  useEffect(() => {
+    setOurNetwork(data.partners_info.map((partner) => partner.country));
+  }, [data]);
 
   const ICAWorld = {
     label: 'Map of ICA Eurasia',
@@ -31,38 +37,42 @@ export const OurGlobalNetwork = () => {
     });
   };
 
+  setClass();
   useEffect(() => {
-    setClass();
     getCountry();
-  }, []);
+  }, [ourNetwork]);
 
   useEffect(() => {
     getPartner();
   }, [country]);
 
-  const getCountry = () => {
-    document.querySelector('.svg-map').addEventListener('mouseover', (e) => {
-      if (e.target.classList.contains('svg-map__location-ica')) {
-        setCountry(e.target.getAttribute('name'));
-      }
-    });
+  const countryNameHandler = (e) => {
+    if (e.target.classList.contains('svg-map__location-ica')) {
+      setCountry(e.target.getAttribute('name'));
+      map && map.removeEventListener('mouseover', countryNameHandler);
+    }
   };
 
-  const getPartner = () => {
+  const getCountry = () => {
+    map && map.addEventListener('mouseover', countryNameHandler);
+  };
+
+  const getPartner = useCallback(() => {
     document.querySelector('.svg-map').addEventListener('click', (e) => {
       if (e.target.classList.contains('svg-map__location-ica')) {
         const partners = data.partners_info.filter((item) => {
           return item.country === country;
         });
+        console.log(partners);
         setCurPartner(partners);
         setShowPartner(true);
-        scrollTo({ id: partners[0].partners_name, duration: 1000 });
+        scrollTo({ id: curPartner.partners_name, duration: 1000 });
       }
     });
-  };
+  }, [country]);
 
   return (
-    <div className="container">
+    <div className='container'>
       <Title>{country}</Title>
       <MapWrapper>
         <SVGMap map={ICAWorld} />
