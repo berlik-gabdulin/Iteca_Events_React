958128e7dc9ca7ccabf5fa2708ea6f9b22afdd79
Berlik Gabdulin <berlik.gabdulin@gmail.com>
2023-05-26 16:13:51 +0600

    Part of code was updates, changes some styles

--- src/pages/eventByIndustry.js
+++ src/pages/eventByIndustry.js
@@ -1,57 +1,130 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
-import styled from "styled-components";
-import { EventCard } from "../components/eventCard";
-import { Loader } from "../components/loader";
-import { EventsService } from "../server/eventsService";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import styled from 'styled-components';
+import { EventCard } from '../components/eventCard';
+import { Loader } from '../components/loader';
+import { EventsService } from '../server/eventsService';
 
 export const EventByIndustry = () => {
-	const dispatch = useDispatch();
-	const { getEventsArrThunk } = EventsService();
-	const fetchStatus = useSelector(({ fetchStatus }) => fetchStatus);
-	const { industry, title } = useParams();
-	const events = useSelector((store) => store.eventsArr);
-	const [eventsToShow, setEventsToShow] = useState([]);
+  const dispatch = useDispatch();
+  const { getEventsArrThunk } = EventsService();
+  const fetchStatus = useSelector(({ fetchStatus }) => fetchStatus);
+  const { industry, title } = useParams();
+  const events = useSelector((store) => store.eventsArr);
+  const [eventsToShow, setEventsToShow] = useState([]);
 
-	useEffect(() => {
-		if (!fetchStatus) {
-			dispatch(getEventsArrThunk());
-		}
-	}, []);
+  useEffect(() => {
+    if (!fetchStatus) {
+      dispatch(getEventsArrThunk());
+    }
+  }, []);
 
-	useEffect(() => {
-		const buffer = events.filter((event) => event.industry === industry);
-		setEventsToShow(buffer);
-		// console.log(eventsToShow);
-	}, [events]);
+  useEffect(() => {
+    const buffer = events.filter((event) => event.industry === industry);
+    setEventsToShow(buffer);
+    // console.log(eventsToShow);
+  }, [events]);
 
-	return (
-		<div className="container">
-			<Title>{title}</Title>
-			<div className="search">
-				<div className="cards">
-					{eventsToShow.length ? (
-						eventsToShow
-							.filter((event) => !event.pastEvent)
-							.map((event, index) => {
-								return <EventCard event={event} key={index} />;
-							})
-					) : (
-						<Loader />
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='container'>
+      <Title>{title}</Title>
+      <div className='search'>
+        <div className='cards'>
+          {eventsToShow.length ? (
+            eventsToShow
+              .filter((event) => !event.pastEvent)
+              .map((event, index) => {
+                return <EventCard event={event} key={index} />;
+              })
+          ) : (
+            <Loader />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const Title = styled.h2`
-	font-size: 32px;
-	text-align: center;
-	margin-bottom: -100px;
-	@media (max-width: 991px) {
-		margin-bottom: -50px;
-		font-size: 24px;
-	}
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: -100px;
+  @media (max-width: 991px) {
+    margin-bottom: -50px;
+    font-size: 24px;
+  }
 `;
+
+// import { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useParams } from 'react-router';
+// import styled from 'styled-components';
+// import { EventCard } from '../components/eventCard';
+// import { Loader } from '../components/loader';
+// import { getEventsArrThunk } from '../server/eventsService';
+
+// export const EventByIndustry = () => {
+//   const dispatch = useDispatch();
+//   const fetchStatus = useSelector((state) => state.fetchStatus);
+//   const events = useSelector((state) => state.eventsArr);
+//   const { industry, title } = useParams();
+//   const [eventsToShow, setEventsToShow] = useState([]);
+
+//   useEffect(() => {
+//     if (!fetchStatus) {
+//       dispatch(getEventsArrThunk());
+//     }
+//   }, [dispatch, fetchStatus]);
+
+//   useEffect(() => {
+//     const buffer = events.filter((event) => event.industry === industry);
+//     setEventsToShow(buffer);
+//   }, [events, industry]);
+
+//   return (
+//     <Container>
+//       <Title>{title}</Title>
+//       <Search>
+//         <Cards>
+//           {eventsToShow.length ? (
+//             eventsToShow
+//               .filter((event) => !event.pastEvent)
+//               .map((event) => <EventCard event={event} key={event.id} />)
+//           ) : (
+//             <Loader />
+//           )}
+//         </Cards>
+//       </Search>
+//     </Container>
+//   );
+// };
+
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+
+// const Title = styled.h2`
+//   font-size: 32px;
+//   text-align: center;
+//   margin-bottom: -100px;
+//   @media (max-width: 991px) {
+//     margin-bottom: -50px;
+//     font-size: 24px;
+//   }
+// `;
+
+// const Search = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+
+// const Cards = styled.div`
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: center;
+//   gap: 20px;
+// `;
--- src/pages/ourGlobalNetwork.js
+++ src/pages/ourGlobalNetwork.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { SVGMap } from 'react-svg-map';
 import 'react-svg-map/lib/index.css';
@@ -10,18 +10,65 @@ import { scrollTo } from '../libs/scrollTo';
 import Members from '../components/members';
 
 export const OurGlobalNetwork = () => {
-  const data = useSelector((state) => {
-    return state.partners;
-  });
+  const data = useSelector((state) => state.partners);
   const [country, setCountry] = useState('Select country');
   const [ourNetwork, setOurNetwork] = useState([]);
   const [showPartner, setShowPartner] = useState(false);
   const [curPartner, setCurPartner] = useState([]);
-  const map = document.querySelector('.svg-map');
+  const mapRef = useRef(null);
 
   useEffect(() => {
     setOurNetwork(data.partners_info.map((partner) => partner.country));
   }, [data]);
+
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map) {
+      const locations = map.querySelectorAll('.svg-map__location');
+      locations.forEach((location) => {
+        const name = location.getAttribute('name');
+        if (ourNetwork.includes(name)) {
+          location.classList.add('svg-map__location-ica');
+        }
+      });
+    }
+  }, [ourNetwork]);
+
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map) {
+      const countryNameHandler = (e) => {
+        if (e.target.classList.contains('svg-map__location-ica')) {
+          setCountry(e.target.getAttribute('name'));
+          map.removeEventListener('mouseover', countryNameHandler);
+        }
+      };
+      map.addEventListener('mouseover', countryNameHandler);
+      return () => {
+        map.removeEventListener('mouseover', countryNameHandler);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map) {
+      const clickHandler = (e) => {
+        if (e.target.classList.contains('svg-map__location-ica')) {
+          const partners = data.partners_info.filter((item) => {
+            return item.country === country;
+          });
+          setCurPartner(partners);
+          setShowPartner(true);
+          scrollTo({ id: curPartner.partners_name, duration: 1000 });
+        }
+      };
+      map.addEventListener('click', clickHandler);
+      return () => {
+        map.removeEventListener('click', clickHandler);
+      };
+    }
+  }, [country]);
 
   const ICAWorld = {
     label: 'Map of ICA Eurasia',
@@ -29,52 +76,10 @@ export const OurGlobalNetwork = () => {
     locations: World.locations.filter((item) => eurasia.includes(item.name)),
   };
 
-  const setClass = () => {
-    [...document.querySelectorAll('.svg-map__location')].map((item) => {
-      if (ourNetwork.includes(item.getAttribute('name'))) {
-        item.classList.add('svg-map__location-ica');
-      }
-    });
-  };
-
-  setClass();
-  useEffect(() => {
-    getCountry();
-  }, [ourNetwork]);
-
-  useEffect(() => {
-    getPartner();
-  }, [country]);
-
-  const countryNameHandler = (e) => {
-    if (e.target.classList.contains('svg-map__location-ica')) {
-      setCountry(e.target.getAttribute('name'));
-      map && map.removeEventListener('mouseover', countryNameHandler);
-    }
-  };
-
-  const getCountry = () => {
-    map && map.addEventListener('mouseover', countryNameHandler);
-  };
-
-  const getPartner = useCallback(() => {
-    document.querySelector('.svg-map').addEventListener('click', (e) => {
-      if (e.target.classList.contains('svg-map__location-ica')) {
-        const partners = data.partners_info.filter((item) => {
-          return item.country === country;
-        });
-        console.log(partners);
-        setCurPartner(partners);
-        setShowPartner(true);
-        scrollTo({ id: curPartner.partners_name, duration: 1000 });
-      }
-    });
-  }, [country]);
-
   return (
     <div className='container'>
       <Title>{country}</Title>
-      <MapWrapper>
+      <MapWrapper ref={mapRef}>
         <SVGMap map={ICAWorld} />
       </MapWrapper>
       {showPartner ? <Partner data={curPartner} /> : null}
